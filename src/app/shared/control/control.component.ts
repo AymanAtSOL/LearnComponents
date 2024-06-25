8f2ec38b2a141e740324621cee40ed600a0b7003
Ayman Alsahwa <ayman.alsahwa@sol.sa>
2024-06-25 14:14:33 +0300

    host property of components with class field

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component, Host, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None, // added the control class to <app-control> as un alternative to :host 
+  host: {
+    class:"control"
+  }
 })
 export class ControlComponent {
   label = input<string>()
